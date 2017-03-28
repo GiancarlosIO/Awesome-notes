ae39e1e22abee684a8a52630f0766996da6523eb
Giancarlos <giancarlos.isasi@gmail.com>
2017-03-28 10:59:40 -0500

    Add styles and link to return to home in 404 page

--- app/javascript/app/components/public-pages/no-found.js
+++ app/javascript/app/components/public-pages/no-found.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
+import Radium from 'radium';
+import Link from '../navigation/link';
 
-export default class NoFound extends Component {
+const styles = {
+  base: {
+    width: '90vw',
+    margin: '0 auto',
+    backgroundColor: '#64C4AF',
+    height: '400px',
+    fontSize: '24px',
+    display: 'flex',
+    flexFlow: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 'calc( 100vh - 550px )'
+  }
+}
+
+class NoFound extends Component {
   render() {
     return (
-      <div>
+      <div style={styles.base}>
         <h1>404-NO MATCH</h1>
+        <div>
+          <Link path="/" noStyles>
+            Return to home
+          </Link>
+        </div>
       </div>
     )
   }
 }
+
+export default Radium()(NoFound);
