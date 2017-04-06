8a5a72b3c1025aaf1234291b3d6eb493fef5825d
Giancarlos Isasi <giancarlos.isasi@gmail.com>
2017-04-05 20:51:39 -0500

    Add ui-icon component

---
+++ app/javascript/app/ui/icons/icon.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Radium from 'radium';
+
+const styles = {
+  base: {
+    margin: '0 10px',
+    width: '20px',
+    borderRadius: '4px',
+    padding: '5px',
+    fontSize: '18px',
+    color: '#16a085',
+    fontWeight: '300',
+    cursor: 'pointer',
+    transition: 'all 0.2s ease',
+    display: 'flex',
+    flexFlow: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    ':hover': {
+      transform: 'scale(1.2)',
+      backgroundColor: 'rgba(0,0,0,0.05)'
+    }
+  }
+}
+
+const Icon = ({type, title, extraStyles, handleClick}) =>
+  (
+    <div style={[styles.base, extraStyles && {...extraStyles}]} title={title} onClick={handleClick}>
+      <i className={`fa fa-${type}`} />
+    </div>
+  )
+
+Icon.Proptypes = {
+  type: React.PropTypes.string.isRequired
+}
+
+export default Radium()(Icon);
